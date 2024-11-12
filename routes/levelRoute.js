718dd3e196c1f8784d99c21e6a4bb53ba096c62b
sd <sdtv9999@gmail.com>
2024-11-13 00:09:06 +0900

    feature:[level-king] 왕퀘스트 api

--- routes/levelRoute.js
+++ routes/levelRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
-	getlevelData,
-	calculateExpRequired,
+    getlevelData,
+    getKingLevelData,
+	calculateExpRequired
 } from '../controllers/levelController.js';
 
 const router = express.Router();
 
 router.get('/', getlevelData);
+router.get('/king', getKingLevelData);
 router.get('/calculate', calculateExpRequired);
 
 export default router;
